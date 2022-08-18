31fe35ab5b6b922a26a98421869ef434952c042c
Yunus Alıcı <yunusac51@gmail.com>
2022-08-18 20:35:21 +0300

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Rick And Morty Project</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-red-600">Rick And Morty</h1>
+    </div>
+  );
 }
 
 export default App;
